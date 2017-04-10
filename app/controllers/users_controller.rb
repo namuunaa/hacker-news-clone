bf42e2ac58b07d8e5bba81aeb5fca752cd28bfa2
Namuun Bayaraa <namuka@gmail.com>
2017-04-10 01:28:09 -0700

    Add route/view for user submissions and comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,22 @@ post '/users' do
   end
 end
 
+# USER PROFILE
 get '/users/:id' do
   @user = User.find(params[:id])
   erb :'users/show'
 end
 
+# USER SUBMISSIONS
+get '/users/:user_id/posts' do
+  @user = User.find(params[:user_id])
+  @posts = @user.posts
+  erb :'posts/index'
+end
+
+# USER COMMENTS
+get '/users/:user_id/comments' do
+  @user = User.find(params[:user_id])
+  @comments = @user.comments
+  erb :'comments/index'
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :comment
+  belongs_to :post
 
   validates :user_id, presence: { message: "You have to be logged in to comment" }
   validates :post_id, presence: { message: "Error, need post id" }
