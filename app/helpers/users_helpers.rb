afbc93ab757b34525f89598adb67ca6b5a80d28c
Namuun Bayaraa <namuka@gmail.com>
2017-04-10 01:08:56 -0700

    Add login, logout, helpers --random bug fixes

---
+++ app/helpers/users_helpers.rb
@@ -0,0 +1,17 @@
+helpers do
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def login_user
+    session[:id] = @user.id
+    redirect '/'
+  end
+
+  def logout_user
+    session[:id] = nil
+    redirect '/'
+  end
+  
+end
