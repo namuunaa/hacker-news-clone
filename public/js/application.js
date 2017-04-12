84be5cc1edf3fc81392019afa7d44e46ae719dd1
Namuun Bayaraa <namuka@gmail.com>
2017-04-12 00:03:09 -0700

    Ajaxify login

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,35 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  userLoginListener();
+
+
 });
+
+
+var userLoginListener = function() {
+  $('#login-link').on('click', function(e){
+    e.preventDefault();
+
+  if($('header').children().length == 3){
+    $('header').find('form').toggle();
+  } else {
+  console.log('in user login listener')
+  var $loginTarget = $(this)
+  var $loginUrl = $(this).attr('href')
+
+  var request = $.ajax({
+    url: $loginUrl,
+    type: 'GET'
+  })
+
+  request.done(function(response) {
+      $('header').append(response);
+  });
+
+  request.fail(function(response) {
+    console.log('ERROR OCCURED IN LOGGIN IN')
+  })
+}
+  })
+}
